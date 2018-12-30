0e1da500c644744df2c375610e8821936fa9234d
dngoyb <dngoyb@gmail.com>
2018-12-30 10:05:45 +0200

    added services for log in

---
+++ client/src/components/Register.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Register</h1>
+        <input type="email" name="email" placeholder="email" v-model="email">
+        <br>
+        <input type="password" name="password" placeholder="password" v-model="password">
+        <br>
+        <button @click="register">Register</button>
+    </div>
+</template>
+
+<script>
+import AuthService from "@/services/AuthService";
+export default {
+    data() {
+        return {
+            email: "abc",
+            password: "abc"
+        };
+    },
+    methods: {
+        async register() {
+            const response = await AuthService.register({
+                email: this.email,
+                password: this.password
+            });
+            console.log(response.data);
+        }
+    }
+};
+</script>
+
+<style>
+</style>
+
